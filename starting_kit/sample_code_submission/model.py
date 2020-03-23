cc29fdb40c41a7267bcd819b5f1b0e272fee8a7c
Emilien <emilien.lem@icloud.com>
2020-03-23 22:31:14 +0100

    Updated model.py

--- starting_kit/sample_code_submission/model.py
+++ starting_kit/sample_code_submission/model.py
@@ -1,7 +1,10 @@
 import numpy as np
+import pickle
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.base import BaseEstimator
+from os.path import isfile
+
 
 class preprocessing ():
     def __init__(self):
@@ -23,6 +26,10 @@ class preprocessing ():
 
 class model (BaseEstimator):
     def __init__(self):
+        '''
+        This constructor is supposed to initialize data members.
+        Use triple quotes for function documentation. 
+        '''
         self.num_train_samples = 38563
         self.num_feat = 59
         self.num_labels = 1
@@ -31,24 +38,68 @@ class model (BaseEstimator):
         self.mod = RandomForestRegressor(n_estimators=60)
 
     def fit(self, X, y):
+        '''
+        This function should train the model parameters.
+        Here we do nothing in this example...
+        Args:
+            X: Training data matrix of dim num_train_samples * num_feat.
+            y: Training label matrix of dim num_train_samples * num_labels.
+        Both inputs are numpy arrays.
+        For classification, labels could be either numbers 0, 1, ... c-1 for c
+        classe or one-hot encoded vector of zeros, with a 1 at the kth position for
+        class k.
+        The AutoML format support on-hot encoding, which also works for
+        multi-labels problems.
+        Use data_converter.convert_to_num() to convert to the category number
+        format.
+        For regression, labels are continuous values.
+        '''
+
+        self.num_train_samples = X.shape[0]
+        if X.ndim > 1: self.num_feat= X.shape[1]
+        print("FIT: dim(X)= [{:d}, {:d}]".format(self.num_train_samples, self.num_feat))
+        num_train_samples = y.shape[0]
+        if y.ndim > 1: self.num_labels = y.shape[1]
+        print("FIT: dim(y)= [{:d}, {:d}]".format(num_train_samples, self.num_labels))
+        if (self.num_train_samples != num_train_samples):
+            print("ARRGH: number of samples in X and y do not match!")
         X_preprocess = self.preprocess.fit_transform(X, y)
         self.mod.fit(X_preprocess, y)
         self.is_trained = True
 
     def predict(self, X):
+        '''
+        This function should provide predictions of labels on (test) data.
+        Here we just return zeros...
+        Make sure that the predicted values are in the correct format for the scoring
+        metric. For example, binary classification problems often expect predictions
+        in the form of a discriminant value (if the area under the ROC curve it the metric)
+        rather that predictions of the class labels themselves. For multi-class or multi-labels
+        problems, class probabilities are often expected if the metric is cross-entropy.
+        Scikit-learn also has a function predict-proba, we do not require it.
+        The function predict eventually can return probabilities.
+        '''
         num_test_samples = X.shape[0]
-        if X.ndim>1: self.num_feat = X.shape[1]
+        if X.ndim > 1: num_feat = X.shape[1]
+        print("PREDICT: dim(X)= [{:d}, {:d}]".format(num_test_samples, num_feat))
+        if (self.num_feat != num_feat):
+            print("ARRGH: number of features in X does not match training data!")
+        print("PREDICT: dim(y)= [{:d}, {:d}]".format(num_test_samples, self.num_labels))
         y = np.zeros([num_test_samples, self.num_labels])
-        
         X_preprocess = self.preprocess.transform(X)
         y = self.mod.predict(X_preprocess)
         return y
 
     def save(self, path="./"):
-        pass
+        pickle.dump(self, open(path + "_model.pickle", "wb"))
 
     def load(self, path="./"):
-        pass
+        modelfile = path + "model.pickle"
+        if isfile(modelfile):
+            with open(modelfile, "rb") as f:
+                self = pickle.load(f)
+                print("Model reloaded from: " + modelfile)
+        return self
 
 def test():
     mod = model()
